aae11682cf9cece5e52366be93d979a8fd5245d3
Saidur KALI <saidurrahman@iut-dhaka.edu>
2024-01-27 20:58:30 +0600

    added comment

--- NavidVaiTask/python1/task2.py
+++ NavidVaiTask/python1/task2.py
@@ -1,8 +1,8 @@
 import requests
 
-url = 'https://www.example.com' 
+url = 'https://www.example.com' #this is the target url
 response = requests.get(url) 
-if response.status_code == 200:  
+if response.status_code == 200:  ## 200 response code indicates that the request has succeed
     headers = response.headers 
     print("Response Headers:")
     for header in headers: 
